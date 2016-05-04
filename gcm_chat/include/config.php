aab17cc17298c18be25de7f736382605f2046f53
Shami <theehtishamshami@live.com>
2016-05-04 09:20:24 -0700

    Lets Start

---
+++ gcm_chat/include/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gcm_chat');
+ 
+define("GOOGLE_API_KEY", "AIzaSyAeOWAsdYi17-Lhb0cc8gQFJtJMN1AjstI");
+ 
+// push notification flags
+define('PUSH_FLAG_CHATROOM', 1);
+define('PUSH_FLAG_USER', 2);
+ 
+?>
